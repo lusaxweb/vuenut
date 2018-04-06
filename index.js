525468a057dca20a72a97436dd70b12ec6939896
luis daniel <luisdanielrovira8@gmail.com>
2018-04-06 11:22:34 -0400

    image

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import componentvuenut from './src/components/vuenut.vue'
-// import './src/css/iconfont/material-icons.css'
-
+// import ''
+require("./src/css/iconfont/material-icons.css");
 const Vuenut = {
   install(Vue, options) {
     Vue.component(componentvuenut.name,componentvuenut)

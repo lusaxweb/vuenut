bf690987cacadf78ed914c1e8be15ed2e8a06ec5
luis daniel <luisdanielrovira8@gmail.com>
2018-04-08 10:13:53 -0400

    cambios en vuenut

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+import components from './App.vue'
+const Vuenut = {
+  theme:{
+    "primary-color":'rgb(51, 186, 77)'
+  },
+  install(Vue, options) {
+      Vue.component(components.name,components)
+  }
+}
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(Vuenut)
+}
+export default Vuenut
